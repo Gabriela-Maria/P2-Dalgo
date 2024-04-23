962562de81ca4262d75aa30732a243ddce14a7a1
MariaaPinzon <ma.pinzonr1@uniandes.edu.co>
2024-04-23 11:33:16 -0500

    ya tenemos costosss

--- P2.py
+++ P2.py
@@ -1,33 +1,49 @@
 import sys
 
 
-class Atomo:
+class Vertice:
     def __init__(self, masa, carga):
         self.masa = masa
         self.carga = carga
 
+    def __repr__(self):
+        return f"Vertice(masa={self.masa}, carga={self.carga})"
+
 class Conexion:
-    def __init__(self, origen, destino):
+    def __init__(self, origen, destino, costo):
         self.origen = origen
         self.destino = destino
-        # falta agregar el atributo costo, calculado xd
+        self.costo=costo
+
+    def __repr__(self):
+        return f"Conexion(origen={self.origen}, destino={self.destino}, costo={self.costo})"   
     
 def cargar_conexiones(data):
-    lines = data.strip().split("\n")
+    lines = data.strip().split("\n") # divide todas las lineas si hay un enter
     index = 0
-    num_cases = int(lines[index])
-    index += 1
+    num_cases = int(lines[index]) #saca la cantidad de casos
+    index += 1 #empieza a contar a partir de la segunda linea
     all_cases = []
     
     for _ in range(num_cases):
         n, w1, w2 = map(int, lines[index].split())  # lee la linea del primer caso
-        print(n)
         index += 1
         conexiones = []
         
         for _ in range(n):
             origen, destino = map(int, lines[index].split())
-            conexion = Conexion(origen, destino)
+
+            masa_origen = abs(origen)
+            carga_origen = "-" if masa_origen <0 else "+"
+            origen = Vertice(masa_origen,carga_origen)
+
+            masa_destino = abs(destino)
+            carga_destino = "-" if masa_destino <0 else "+"
+            destino = Vertice(masa_destino,carga_destino)
+
+            costo =  (1 + abs(w1-w2) % w1) if (carga_origen == carga_destino) else (w2 - abs(w1-w2) % w2)
+
+            conexion = Conexion(origen, destino,costo) # falta añadir el costo
             conexiones.append(conexion)
             index += 1
         
@@ -37,15 +53,15 @@ def cargar_conexiones(data):
 
 # Example usage
 if __name__ == "__main__":
-    data = """2
-3 3 5
-1 3
--6 3
-1 7
-3 2 3
-1 2
--2 3
-3 -4"""
+    data = """  2
+                3 3 5
+                1 3
+                -6 3
+                1 7
+                3 2 3
+                1 2
+                -2 3
+                3 -4"""
     result = cargar_conexiones(data)
     for case_conexiones in result:
         print(case_conexiones)
